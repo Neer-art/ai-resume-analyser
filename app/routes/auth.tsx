b7c67e0d2d19eb0cbea4c3c96342a7a7919155a5
Neeraj <neerajnanwal17@gmail.com>
2026-04-22 18:51:11 +0530

    authentictaion done

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { usePuterStore } from '~/lib/putter'
+import {useLocation, useNavigate} from "react-router"
 
-export const meta = () => ([
-  { title: 'Resumind | auth' },
-  { name: 'description', content: 'Authentication page' }
-]);
 
-const Auth = () => {
-  return (
-    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen">
-      <div className="gradient-border shadow-lg">
-        <section className="flex flex-col gap-8 bg-white rounded-lg p-8">
-          <div className="flex flex-col gap-2 items-center text-center">
-            <h1>Welcome</h1>
-            <h2>Log in to continue your job journey</h2>
-          </div>
-        </section>
-      </div>
-    </main>
-  );
-};
 
-export default Auth;
+const meta = () => ([
+    { title: "Resumind | Auth" },
+    { name: "description", content: "Log into your account " },
+])
+
+const auth = () => {
+
+    const { isLoading,auth  } = usePuterStore();
+    const location = useLocation()
+    const next = location.search.split('next=')[1] || '/'    
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (isLoading && auth.isAuthenticated) navigate(next);
+        // if (!isLoading && auth.isAuthenticated) {
+        //     return null;
+        }, [auth.isAuthenticated, isLoading,navigate,next])
+
+
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className='gradient-border shadow-lg'>
+                <section className="bg-white flex flex-col gap-8 rounded-2xl p-10">
+                    <div className= "flex flex-col items-center gap-2 text-center " >
+                        <h1> Welcome </h1>
+                        <h2>Log into Continue Your Job Journey</h2>
+
+                    </div>
+                    <div>
+                        {isLoading ?(
+                            <button className='auth-button animate-pulse'>
+                                <p> Signing you in ...</p>
+                            </button>
+                        ) : (
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className='auth-button' onClick={() => auth.signOut()}>
+                                        <p> Log Out </p>
+                                    </button> 
+                                ) : (
+                                    <button className='auth-button' onClick={() => auth.signIn()}>
+                                        <p> Log in  </p>
+                                    </button> 
+                                )}
+                            </>
+                        )}
+                    </div>
+                </section>
+            </div>
+        </main>
+    
+  )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,10 @@ import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import ResumeCard from "~/component/ResumeCard";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/putter";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +15,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+  const { auth  } = usePuterStore();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
